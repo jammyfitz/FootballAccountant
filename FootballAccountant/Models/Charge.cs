19c68035a61b40ffa64feaa2a46f8aae455a8a10
Jamie Fitzgerald <jammyfitz@hotmail.com>
2016-07-04 12:04:07 +0100

    Adding CSS and some missing files from initial commit

--- FootballAccountant/Models/Charge.cs
+++ FootballAccountant/Models/Charge.cs
@@ -5,7 +5,6 @@ namespace FootballAccountant.Models
 {
     public class Charge
     {
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yy}")]
         public DateTime Date { get; set; }
         public decimal Cost { get; set; }
         public string Notes { get; set; }
